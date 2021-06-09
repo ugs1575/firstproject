25e865c8611983ceb3dd5b125ea95600f3d59043
Gyeongseo Woo <ugs1575@gmail.com>
2021-06-09 10:58:55 +0900

    210609 header, footer 파일 분리

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,49 +17,23 @@
         rel="stylesheet" 
         href="//use.fontawesome.com/releases/v5.0.7/css/all.css"
     />
+    <style>
+        .active {
+            color: red;
+        }
+    </style>
 </head>
 
 <body>
     <header>
-        <!-- Navigation -->
-        <nav class="top-menu-container">
-            <div class="logo-header">
-                <a href="">
-                    <img 
-                    src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png" 
-                    alt="Logo personal portfolio"
-                    title="Logo personal portfolio"
-                    />
-                </a>
-            </div>
-
-            <ul>
-                <li>
-                    <a href="">About</a>
-                </li>
-                <li>
-                    <a href="">Portfolio</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
-            </ul>
-        </nav>
+        @include('layouts.header')
     </header>
 
     @yield('content')
 
     <!-- Footer -->
     <footer>
-        <div class="container-footer">
-            <img 
-                src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png" 
-                alt="Logo personal portfolio"
-                title="Logo personal portfolio"
-                />
-            <p>
-                This website is created by Code With Dary
-            </p>
+        @include('layouts.footer')
     </footer>
 </body>
 </html>
